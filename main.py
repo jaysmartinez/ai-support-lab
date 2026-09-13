dfd9b92d0e03570f7c0424fc9bc780b010ebca16
Jay Martinez <jaysmartinez@gmail.com>
2026-09-12 20:08:59 -0500

    Allow local frontend access with CORS

--- main.py
+++ main.py
@@ -4,7 +4,17 @@ from routers.tickets import router as tickets_router
 from routers.auth import router as auth_router
 from routers.customers import router as customers_router
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=False,
+    allow_methods=["GET", "POST"],
+    allow_headers=["Content-Type"],
+)
 
 app.include_router(tickets_router)
 app.include_router(auth_router)
